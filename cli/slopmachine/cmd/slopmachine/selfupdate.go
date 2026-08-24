0a299abef31215b826b30eb16733d4ee2baa696e
Altay <altay@hey.com>
2026-08-24 10:36:25 +0300

    refactor(cli): share self-update implementation

--- cli/slopmachine/cmd/slopmachine/selfupdate.go
+++ cli/slopmachine/cmd/slopmachine/selfupdate.go
@@ -8,9 +8,9 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/uinaf/ffss/cli/lib/selfupdate"
 	"github.com/uinaf/ffss/cli/slopmachine/internal/buildinfo"
 	"github.com/uinaf/ffss/cli/slopmachine/internal/machine"
-	"github.com/uinaf/ffss/cli/slopmachine/internal/selfupdate"
 )
 
 // cmdSelfupdate replaces this binary with a published release. It shares
